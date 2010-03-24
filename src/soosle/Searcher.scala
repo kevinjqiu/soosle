9b235cf46df6f246191bb2ac2b88b88e198b100b
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2010-03-23 23:16:42 -0400

    mostly working except word location

--- src/soosle/Searcher.scala
+++ src/soosle/Searcher.scala
@@ -5,17 +5,30 @@ import com.db4o.query.{Predicate, Query}
 
 class Searcher(val dbname:String) extends NeedsDbConnection(dbname) {
 
-  def query(queryStr:String) = {
+  def query(queryStr:String):List[WordLocation] = {
+    var retval:List[WordLocation] = List()
+    
     withConnection {
       dbConn => {
-        var re = queryStr.split("""\b+""").map(".*" + _ + ".*").mkString(" | ")
+        var re = queryStr.split("""\b+""").filter(_ != "").map(".*" + _ + ".*").mkString(" | ")
+        println("re=" + re)
+
         var result = dbConn.query(new Predicate[WordLocation] {
           def `match`(wl:WordLocation):Boolean = {
-            false // TODO
+            return wl.word.matches(re)
           }
         })
+
+        // Make a copy of the result set
+        // since the connection is going to be closed
+        // the copied result set is going to be returned
+        // XXX: more idiomatic way?
+        for (i<-0 until result.size)
+          retval = result.get(i) :: retval
       }
     }
+
+    retval
   }
 
 }
--- src/soosle/Soosle.scala
+++ src/soosle/Soosle.scala
@@ -4,10 +4,14 @@ import scala.collection.mutable.Set
 
 object Soosle {
   def main(args:Array[String]) {
-    val crawler = new Crawler("soosle.db", Set("http://kiwitobes.com/wiki/Programming_language.html"), 2)
-    crawler.crawl
+//    val crawler = new Crawler("soosle.db", Set("http://kiwitobes.com/wiki/Programming_language.html"), 2)
+//    crawler.crawl
 
     val searcher = new Searcher("soosle.db")
-    println(searcher.search("java"))
+    searcher.query("language").foreach {
+      result => {
+        println("" + result.word + " found in page " + result.pageUrl + ":" + result.location)
+      }
+    }
   }
 }
